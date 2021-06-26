764f78adaff237c4dde520fbff6bbf29fe7deda3
Adrien MOREAU <moreauadrien92@gmail.com>
2021-06-26 23:18:24 +0200

    'commit'

--- googlerandom.js
+++ googlerandom.js
@@ -57,7 +57,7 @@ document.head.innerHTML += `
         }
         .setmode > .button {
             width: 85%;
-            font-size: 16px;
+            font-size: 24px;
         }
 
     </style>
