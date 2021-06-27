4ee6f3518c6f82d5ec1450d9367f729efbc3d042
Adrien MOREAU <moreauadrien92@gmail.com>
2021-06-27 16:44:13 +0200

    'commit'

--- googlerandom.js
+++ googlerandom.js
@@ -16,220 +16,223 @@ const stack = {
 
 // Create stylesheet
 document.head.innerHTML += `
-        <style>
-            .popup {
-                width: 100%;
-                height: 100%;
-                background-color: rgba(0, 0, 0, .4);
-                text-align: center;
-                position: fixed;
-                top: 0;
-                z-index: 10;
-            }
-            .content {
-                position: absolute;
-                width: 85%;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                background-color: #fff;
-                border-radius: 8px;
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                padding: 1.25rem;
-            }
-            .button {
-                background-color: #fff;
-                border-color: #dbdbdb;
-                border-width: 1px;
-                border-radius: 4px;
-                color: #363636;
-                cursor: pointer;
-                justify-content: center;
-                padding-bottom: calc(.5em - 1px);
-                padding-left: 1em;
-                padding-right: 1em;
-                padding-top: calc(.5em - 1px);
-                text-align: center;
-                white-space: nowrap;
-            }
-            .is-primary {
-                background-color: #00d1b2;
-                border-color: transparent;
-                color: #fff;
-            }
-            .is-info {
-                background-color: #3e8ed0;
-                border-color: transparent;
-                color: #fff;
-            }
-            .is-rounded {
-                border-radius: 9999px;
-                padding-left: calc(1em + .25em);
-                padding-right: calc(1em + .25em);
-            }
-            .hidden {
-                display: none;
-            }
-            .setmode{
-                width: 80%;
-                font-size: 16px;
-            }
-            .values {
-                display: grid;
-                grid-template-columns: repeat(4,1fr);
-                grid-gap: 10px;
-            }
-            .values > .button {
-                font-size: 16px;
-                background-color: #ebfffc;
-                color: #00947e;
-                border: 1px #00d1b2 solid;
-            }
-            .values > .button:last-child {
-                grid-column: 2/4;
-            }
-            button {
-                outline: 0;
-            }
-            .suits {
-                display: grid;
-                grid-template-columns: repeat(2,1fr);
-                grid-gap: 10px;
-            }
-            .suits > .button {
-                font-size: 16px;
-                background-color: #e3ecfa;
-                color: #2160c4;
-                border: 1px #3273dc solid;
-            }
-            .validate {
-                background-color: #3ec46d;
-                color: white;
-                font-size: 16px;
-                width: 70%;
-                margin: auto !important;
-            }
+    <style>
+        .popup {
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, .4);
+            text-align: center;
+            position: fixed;
+            top: 0;
+            z-index: 10;
+        }
+        .content {
+            position: absolute;
+            width: 85%;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: #fff;
+            border-radius: 8px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 1.25rem;
+        }
+        .button {
+            background-color: #fff;
+            border-color: #dbdbdb;
+            border-width: 1px;
+            border-radius: 4px;
+            color: #363636;
+            cursor: pointer;
+            justify-content: center;
+            padding-bottom: calc(.5em - 1px);
+            padding-left: 1em;
+            padding-right: 1em;
+            padding-top: calc(.5em - 1px);
+            text-align: center;
+            white-space: nowrap;
+        }
+        .is-primary {
+            background-color: #00d1b2;
+            border-color: transparent;
+            color: #fff;
+        }
+        .is-info {
+            background-color: #3e8ed0;
+            border-color: transparent;
+            color: #fff;
+        }
+        .is-rounded {
+            border-radius: 9999px;
+            padding-left: calc(1em + .25em);
+            padding-right: calc(1em + .25em);
+        }
+        .hidden {
+            display: none;
+        }
+        .setmode{
+            width: 80%;
+            font-size: 16px;
+        }
+        .values {
+            display: grid;
+            grid-template-columns: repeat(4,1fr);
+            grid-gap: 10px;
+        }
+        .values > .button {
+            font-size: 16px;
+            background-color: #ebfffc;
+            color: #00947e;
+            border: 1px #00d1b2 solid;
+        }
+        .values > .button:last-child {
+            grid-column: 2/4;
+        }
+        button {
+            outline: 0;
+        }
+        .suits {
+            display: grid;
+            grid-template-columns: repeat(2,1fr);
+            grid-gap: 10px;
+        }
+        .suits > .button {
+            font-size: 16px;
+            background-color: #e3ecfa;
+            color: #2160c4;
+            border: 1px #3273dc solid;
+        }
+        .validate {
+            background-color: #3ec46d;
+            color: white;
+            font-size: 16px;
+            width: 70%;
+            margin: auto !important;
+        }
 
-            #valueSelected {
-                background-color: #00d1b2;
-                border: none;
-                color: #fff;
-            }
-            #suitSelected {
-                background-color: #485fc7;
-                border: none;
-                color: #fff;
-            }
-            .notification {
-                font-size: 16px;
-                background-color: #f14668;
-                color: #fff;
-                margin-bottom: 1.5rem;
-                border-radius: 4px;
-                position: relative;
-                padding: 1.25rem 2.5rem 1.25rem 1.5rem;
-                font-family: "Segoe UI";
-            }
-            .number-selector {
-                width: 100%;
-            }
-            
-            .input {
-                -moz-appearance: none;
-                -webkit-appearance: none;
-                align-items: center;
-                border: 1px solid transparent;
-                border-radius: 4px;
-                box-shadow: none;
-                display: inline-flex;
-                font-size: 1rem;
-                height: 2.5em;
-                justify-content: flex-start;
-                line-height: 1.5;
-                padding-bottom: calc(.5em - 1px);
-                padding-left: calc(.75em - 1px);
-                padding-right: calc(.75em - 1px);
-                padding-top: calc(.5em - 1px);
-                position: relative;
-                vertical-align: top;
-            }
-            .input {
-                border-color: #b5b5b5;
-                outline: 0;
-                box-shadow: inset 0 .0625em .125em rgba(10,10,10,.05);
-                width: 80%;
-                background-color: #fff;
-                border-radius: 4px;
-                color: #363636;
-            }
-            
-        </style>
-    `
+        #valueSelected {
+            background-color: #00d1b2;
+            border: none;
+            color: #fff;
+        }
+        #suitSelected {
+            background-color: #485fc7;
+            border: none;
+            color: #fff;
+        }
+        .notification {
+            font-size: 16px;
+            background-color: #f14668;
+            color: #fff;
+            margin-bottom: 1.5rem;
+            border-radius: 4px;
+            position: relative;
+            padding: 1.25rem 2.5rem 1.25rem 1.5rem;
+            font-family: "Segoe UI";
+        }
+        .number-selector {
+            width: 100%;
+        }
+        
+        .input {
+            -moz-appearance: none;
+            -webkit-appearance: none;
+            align-items: center;
+            border: 1px solid transparent;
+            border-radius: 4px;
+            box-shadow: none;
+            display: inline-flex;
+            font-size: 1rem;
+            height: 2.5em;
+            justify-content: flex-start;
+            line-height: 1.5;
+            padding-bottom: calc(.5em - 1px);
+            padding-left: calc(.75em - 1px);
+            padding-right: calc(.75em - 1px);
+            padding-top: calc(.5em - 1px);
+            position: relative;
+            vertical-align: top;
+        }
+        .input {
+            border-color: #b5b5b5;
+            outline: 0;
+            box-shadow: inset 0 .0625em .125em rgba(10,10,10,.05);
+            width: 80%;
+            background-color: #fff;
+            border-radius: 4px;
+            color: #363636;
+        }
+        
+    </style>
+`
 // Create popup
 document.body.innerHTML += `
-        <div class="popup">
-            <div class="content">
-                <div class="setmode">
-                    <div class="button is-rounded is-primary">Stack</div>
-                    <br/>
-                    <br/>
-                    <div class="button is-rounded is-info">Number</div>
+    <div class="popup">
+        <div class="content">
+            <div class="setmode">
+                <div class="button is-rounded is-primary">Stack</div>
+                <br/>
+                <br/>
+                <div class="button is-rounded is-info">Number</div>
+            </div>
+
+            <div class="stack-selector hidden">
+                <div class="values">
+                    <div class="button" valueCode="A">Ace</div>
+                    <div class="button" valueCode="2">Two</div>
+                    <div class="button" valueCode="3">Three</div>
+                    <div class="button" valueCode="4">Four</div>
+                    <div class="button" valueCode="5">Five</div>
+                    <div class="button" valueCode="6">Six</div>
+                    <div class="button" valueCode="7">Seven</div>
+                    <div class="button" valueCode="8">Eight</div>
+                    <div class="button" valueCode="9">Nine</div>
+                    <div class="button" valueCode="10">Ten</div>
+                    <div class="button" valueCode="J">Jack</div>
+                    <div class="button" valueCode="Q">Queen</div>
+                    <div class="button" valueCode="K">King</div>
                 </div>
 
-                <div class="stack-selector hidden">
-                    <div class="values">
-                        <div class="button" valueCode="A">Ace</div>
-                        <div class="button" valueCode="2">Two</div>
-                        <div class="button" valueCode="3">Three</div>
-                        <div class="button" valueCode="4">Four</div>
-                        <div class="button" valueCode="5">Five</div>
-                        <div class="button" valueCode="6">Six</div>
-                        <div class="button" valueCode="7">Seven</div>
-                        <div class="button" valueCode="8">Eight</div>
-                        <div class="button" valueCode="9">Nine</div>
-                        <div class="button" valueCode="10">Ten</div>
-                        <div class="button" valueCode="J">Jack</div>
-                        <div class="button" valueCode="Q">Queen</div>
-                        <div class="button" valueCode="K">King</div>
-                    </div>
+                <br/>
+                <br/>
 
-                    <br/>
-                    <br/>
-
-                    <div class="suits">
-                        <div class="button" suitCode="s">Spades</div>
-                        <div class="button" suitCode="h">Hearts</div>
-                        <div class="button" suitCode="c">Clubs</div>
-                        <div class="button" suitCode="d">Diamonds</div>
-                    </div>
-
-                    <br/>
-                    <br/>
-
-                    <div class="notification hidden">
-                        You must choose a value and a suit
-                    </div>
-
-                    <div class="button validate">Validate</div>
+                <div class="suits">
+                    <div class="button" suitCode="s">Spades</div>
+                    <div class="button" suitCode="h">Hearts</div>
+                    <div class="button" suitCode="c">Clubs</div>
+                    <div class="button" suitCode="d">Diamonds</div>
                 </div>
 
-                <div class="number-selector hidden">
-                    <input type="tel" class="input" placeholder="Number you want to force">
+                <br/>
+                <br/>
 
-                    <br/>
-                    <br/>
-
-                    <div class="notification hidden">
-                        You must enter a valid number
-                    </div>
-
-                    <div class="button validate">Validate</div>
+                <div class="notification hidden">
+                    You must choose a value and a suit
                 </div>
+
+                <div class="button validate">Validate</div>
+            </div>
+
+            <div class="number-selector hidden">
+                <input type="tel" class="input" placeholder="Number you want to force">
+
+                <br/>
+                <br/>
+
+                <div class="notification hidden">
+                    You must enter a valid number
+                </div>
+
+                <div class="button validate">Validate</div>
             </div>
         </div>
-    `
+    </div>
+`
+
+
+const resetStyle = () => document.head.removeChild(document.head.lastChild)
 
 const setmodeDiv = document.querySelector('.setmode')
 setmodeDiv.querySelectorAll('.button').forEach(elem => {
@@ -286,6 +289,8 @@ const getBoundaries = () => {
     const getBoundary = (bound) => parseInt(document.querySelector(`.gws-csf-randomnumber__${bound}Val`).value)
     return { min: getBoundary('min'), max: getBoundary('max') }
 }
+
+
 
 const disableButton = () => {
     for (let i = 0; button.attributes.length > 0; i++) button.removeAttribute(button.attributes[0].name)
